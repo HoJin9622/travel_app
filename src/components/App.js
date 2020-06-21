ed0ee0e43613584a6e233110f97844297c1cec77
HoJin9622 <kiss0104040@gmail.com>
2020-06-21 22:40:38 +0900

    Add Navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import styled from "styled-components";
+import Navigation from "./Navigation";
+
+const Background = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-image: url("/images/bg.png");
+  background-size: cover;
+  background-position: center center;
+`;
 
 function App() {
-  return <div>Initial</div>;
+  return (
+    <>
+      <Background>
+        <Navigation />
+      </Background>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,70 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 42px 240px 0 240px;
+`;
+
+const NavigationContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%%;
+  height: 10%;
+  padding: 0 0 29px 0;
+`;
+
+const SideNavi = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 15%;
+`;
+
+const Title = styled.h1`
+  font-family: SF Pro Display;
+  font-size: 24px;
+  line-height: 29px;
+  letter-spacing: 0.01em;
+  color: #ffffff;
+`;
+
+const NaviLetter = styled.h4`
+  font-family: SF Pro Text;
+  font-size: 18px;
+  line-height: 21px;
+  color: #ffffff;
+`;
+
+const NavigationBottomLine = styled.div`
+  width: 100%;
+  height: 2px;
+  background: #e7e7f2;
+  opacity: 0.4;
+  backdrop-filter: blur(60px);
+  /* Note: backdrop-filter has minimal browser support */
+  transform: matrix(1, 0, 0, -1, 0, 0);
+`;
+
+function Navigation() {
+  return (
+    <Container>
+      <NavigationContainer>
+        <SideNavi>
+          <NaviLetter>About</NaviLetter>
+          <NaviLetter>Contact</NaviLetter>
+        </SideNavi>
+        <Title>Camping.</Title>
+        <SideNavi>
+          <NaviLetter>Sign In</NaviLetter>
+          <NaviLetter>Sign Up</NaviLetter>
+        </SideNavi>
+      </NavigationContainer>
+      <NavigationBottomLine />
+    </Container>
+  );
+}
+
+export default Navigation;
