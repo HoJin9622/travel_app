c74c0b23c22ac35c124f523bc72bd88cfb707362
HoJin9622 <kiss0104040@gmail.com>
2020-06-29 22:27:12 +0900

    Updates

---
+++ precache-manifest.430af6db3b32c277f53ddc082e43634b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "184449e60128a81fe25c1a73d8cd1e38",
+    "url": "/travel_app/index.html"
+  },
+  {
+    "revision": "7ac04ad4f5ee13b43cd8",
+    "url": "/travel_app/static/js/2.76f2207b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/travel_app/static/js/2.76f2207b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f765ff9778caa1c33d2a",
+    "url": "/travel_app/static/js/main.f9a776ad.chunk.js"
+  },
+  {
+    "revision": "d240c30104bde046ae22",
+    "url": "/travel_app/static/js/runtime-main.9bfd6b33.js"
+  }
+]);
